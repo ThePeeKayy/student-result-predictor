753b843b302dacac9225df0f6ccc65a14d3b50ed
The Infinity <chunyin0309@hotmail.com>
2024-04-08 15:19:15 +0800

    trial

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ from sklearn.metrics import mean_squared_error
 import numpy as np
 
 # Load the dataset
-data = pd.read_csv('path_to_your_csv_file.csv')
+data = pd.read_csv('StudentsPerformance.csv')
+df = pd.DataFrame(data)
+columns_to_drop = ['lunch', 'race/ethnicity']
+df = df.drop(columns=columns_to_drop, axis=1)
 
 # Prepare the data
-X = data.drop('result_column_name', axis=1)  # Assuming 'result_column_name' is the column to predict
-y = data['result_column_name']
+X = df.drop('math score', axis=1)  # Assuming 'result_column_name' is the column to predict
+y = df['math score']
 
 # Split the data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
