05c10157de56b52fffd19e507ebff8b689861d82
pengkiang <pengkiangtan@gmail.com>
2024-04-11 19:32:37 +0800

    web

--- predictorwebsite/app/page.js
+++ predictorwebsite/app/page.js
@@ -10,6 +10,18 @@ const LunchOption = [
   { id: 2, name: 'Free/Reduced' },
 
 ]
+
+const TestOption = [
+  { id: 1, name: 'completed' },
+  { id: 2, name: 'none' },
+
+]
+
+const GenderOption = [
+  { id: 1, name: 'Male' },
+  { id: 2, name: 'Female' },
+
+]
 const EducationOption = [
   { id: 1, name: 'Wade Cooper' },
   { id: 2, name: 'Arlene Mccoy' },
@@ -30,7 +42,7 @@ const RaceOption = [
 export default function Home() {
   return (
     <main 
-    className="bg-gray-900 flex overflow-hidden h-[100vh] justify-center"
+    className="bg-gray-900 flex overflow-auto lg:overflow-hidden lg:h-[100vh] justify-center"
     >
       <motion.div
       variants={staggerContainer}
@@ -39,12 +51,12 @@ export default function Home() {
       viewport={{once:false,amount:0.25}}
       className={`bg-gray-800 bg-opacity-95 rounded-lg p-4 max-w-[1000px] shadow-lg my-8 w-[92vw] flex space-y-4 flex-col`}
       >
-        <div className='flex flex-row space-x-4 ml-[70px]'>
-        <Image width={90} height={50} src='/Our-story.png'/>
-      <motion.p 
-        variants={textContainer}
-        className={`font-bold flex flex-row flex-wrap text-transparent bg-gradient-to-r from-red-400 to-yellow-300 bg-clip-text text-[50px] `}
-      >
+        <div className='flex flex-row space-x-4 ml-[30px] lg:ml-[70px]'>
+        <Image className='hidden lg:block' width={90} height={50} src='/Our-story.png'/>
+        <motion.p 
+          variants={textContainer}
+          className={`font-bold flex flex-row flex-wrap text-transparent bg-gradient-to-r from-red-400 to-yellow-300 bg-clip-text text-[50px] `}
+        >
         {Array.from('Student Result Predictor').map((letter,i) => (
           <motion.span variants ={textVariant2} className='flex flex-wrap whitespace-pre-wrap' key={i}>
             {letter === ' '?'\u00A0' :letter}
@@ -55,7 +67,18 @@ export default function Home() {
       </div>
         <p className='text-white px-16 text-lg text-justify font-sans'>Explore the future of education with our innovative math score prediction bot. Using cutting-edge technology, our bot analyzes reading and writing scores along with parental education levels to make precise predictions, helping students and educators alike excel.</p>
       <form className='gap-2 flex-col flex px-16'>
-      <input
+        <div className='lg:flex-row lg:gap-x-3 flex-col flex'>
+          <div className='flex flex-col lg:w-[50%] w-full space-y-2'>
+            <SelectMenu title={'Education level'} options={EducationOption}/>
+            <SelectMenu title={'Gender'} options={GenderOption}/>
+            <SelectMenu title={'Test Prep'} options={TestOption}/>
+          </div>
+          <div className='flex flex-col lg:w-[50%] w-full space-y-2'> 
+            <SelectMenu title={'Race Group'} options={RaceOption}/>
+            <SelectMenu title={'Lunch Option'} options={LunchOption}/>
+          </div>
+        </div>
+        <input
           type="number"
           name="reading score"
           id="reading score"
@@ -69,9 +92,7 @@ export default function Home() {
           className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           placeholder="Writing score"
         />
-        <SelectMenu title={'Education level'} options={EducationOption}/>
-        <SelectMenu title={'Race Group'} options={RaceOption}/>
-        <SelectMenu title={'Lunch Option'} options={LunchOption}/>
+        
       </form>
       <div className='flex justify-center'>
         <div className='bg-white font-bold font-sans rounded-md w-[30vw] min-w-[100px] p-2 ring-[2px] ring-gray-500 shadow-xl'>
