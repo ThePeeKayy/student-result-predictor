58b630bd5e84bd12675437d8ebd108f53866387a
pengkiang <pengkiangtan@gmail.com>
2024-04-12 15:33:02 +0800

    change url

--- predictorwebsite/app/page.js
+++ predictorwebsite/app/page.js
@@ -51,7 +51,7 @@ export default function Home() {
     const [educationOption,setEducationOption] = useState('high school')
     const [mathResult,setMathResult] = useState(null)
     const handleSubmit = async () => {
-      const response = await fetch('http://127.0.0.1:8080/bot', {
+      const response = await fetch('https://student-result-predictor.vercel.app/bot', {
         method:'POST',
         headers: {
           'Content-type':'application/json'
